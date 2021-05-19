faa93e310d1fd53801ed58021215058aef1524e4
Ujang Riswanto <ujangriswanto08@gmail.com>
2021-05-19 22:46:20 +0700

    cobaan test nung

--- evaluasi/login.php
+++ evaluasi/login.php
@@ -1,5 +1,26 @@
 <?php 
+session_start();
+if (isset($_POST['tdaftar'])) {
+    include "koneksi.php";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
+    $cek_user = mysqli_query($koneksi,"SELECT * FROM tdaftar WHERE username = '$username'");
+    $row = mysqli_num_rows($cek_user);
+
+    if ($row === 1) {
+        $fetch_pass = mysqli_fetch_assoc($cek_user);
+        $cek_pass = $fetch_pass['password'];
+        if ($cek_pass <> $password) {
+            echo "<script>alert('Password salah'); </script>";
+        }else{
+            $_SESSION['log'] = true;
+            echo "<script>alert('Login berhasil'); document.location.href='utama.php'</script>";
+        }
+    }else{
+        echo "<script>alert('Username salah atau belum terdaftar');</script>";
+        }
+    }
 
 
  ?>
